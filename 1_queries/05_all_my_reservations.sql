ce11487b2638ea865de16b5e0232c9058ab7cca3
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-02-20 10:37:07 -0500

    refactor SELECT queries

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -1,8 +1,8 @@
-SELECT reservations.id as id, properties.title as title, properties.cost_per_night, reservations.start_date, avg(property_reviews.rating) as average_rating 
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(property_reviews.rating) as average_rating 
 FROM reservations
 JOIN properties ON properties.id = reservations.property_id
 JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
-GROUP BY reservations.id, properties.title, properties.cost_per_night 
+GROUP BY properties.id, reservations.id 
 ORDER BY reservations.start_date
-LIMIT 10;
+LIMIT 10; 
